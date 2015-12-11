a4b6b12bbd42a0f3252264db4941f456805bfa6d
Derek Pryor <Derek.Pryor@jhuapl.edu>
2015-12-11 14:12:52 -0500

    implemented argparse for cloudformation.py

--- cloud_formation/cloudformation.py
+++ cloud_formation/cloudformation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
+import os
 import importlib
 from boto3.session import Session
 import json
 import pprint
+import glob
 
 import hosts
 
@@ -28,28 +31,48 @@ def generate_config(domain, config):
     module = importlib.import_module("configs." + config)
     module.generate("templates", domain)
 
-def usage():
-    print("Usage: {} <aws-credentials> (create|generate) <domain_name> <config_name>".format(sys.argv[0]))
-    sys.exit(1)
-
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
-        usage()
-        
-    cred_file = sys.argv[1]
-    action = sys.argv[2]
-    domain_name = sys.argv[3]
-    config_name = sys.argv[4]
+    def create_help(header, options):
+        return "\n" + header + "\n" + \
+               "\n".join(map(lambda x: "  " + x, options)) + "\n"
+
+    config_names = [x.split('/')[1].split('.')[0] for x in glob.glob("configs/*.py") if "__init__" not in x]
+    config_help = create_help("config_name supports the following:", config_names)
     
-    with open(cred_file, "r") as fh:
-        credentials = json.load(fh)
+    actions = ["create", "generate"]
+    actions_help = create_help("action supports the following:", actions)
+
+    parser = argparse.ArgumentParser(description = "Script the creation and provisioning of CloudFormation Stacks",
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=actions_help + config_help)
+    parser.add_argument("--aws-credentials", "-a",
+                        metavar = "<file>",
+                        default = os.environ.get("AWS_CREDENTIALS"),
+                        type = argparse.FileType('r'),
+                        help = "File with credentials to use when connecting to AWS (default: AWS_CREDENTIALS)")
+    
+    parser.add_argument("action",
+                        choices = ["create","generate"],
+                        metavar = "action",
+                        help = "Action to execute")
+    parser.add_argument("domain_name", help="Domain in which to execute the configuration (example: subnet.vpc.boss)")
+    parser.add_argument("config_name",
+                        choices = config_names,
+                        metavar = "config_name",
+                        help="Configuration to act upon (imported from configs/)")
+
+    args = parser.parse_args()
+    
+    if args.aws_credentials is None:
+        print("Error: AWS credentials not provided and AWS_CREDENTIALS is not defined")
+        sys.exit(1)
+    
+    credentials = json.load(args.aws_credentials)
     
     session = create_session(credentials)
 
-    if action in ("create", ):
-        create_config(session, domain_name, config_name)
-    elif action in ("generate", "gen"):
-        generate_config(domain_name, config_name)
-    else:
-        usage()
+    if args.action in ("create", ):
+        create_config(session, args.domain_name, args.config_name)
+    elif args.action in ("generate", "gen"):
+        generate_config(args.domain_name, args.config_name)
     
