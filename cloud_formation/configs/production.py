a84fc64d7603b3bfe5580ea9e435046a8671dff6
Tim Gion <timothy.gion@jhuapl.edu>
2016-01-22 13:57:05 -0500

    Fixed logic error and typo. Properly disabled creating PKI backend while waiting for certs. Fixed typo in delcaration of kwargs parameter.

--- cloud_formation/configs/production.py
+++ cloud_formation/configs/production.py
@@ -23,7 +23,7 @@ ADDRESSES = {
     "endpoint": range(10, 20),
     "db": range(20, 30),
 }
-       
+
 def create_config(session, domain, keypair=None, user_data=None, db_config={}):
     """Create the CloudFormationConfiguration object."""
     config = configuration.CloudFormationConfiguration(domain, ADDRESSES)
@@ -31,42 +31,42 @@ def create_config(session, domain, keypair=None, user_data=None, db_config={}):
     # do a couple of verification checks
     if config.subnet_domain is not None:
         raise Exception("Invalid VPC domain name")
-        
+
     vpc_id = lib.vpc_id_lookup(session, domain)
     if session is not None and vpc_id is None:
         raise Exception("VPC does not exists, exiting...")
-        
+
     # Lookup the VPC, External Subnet, Internal Security Group IDs that are
     # needed by other resources
     config.add_arg(configuration.Arg.VPC("VPC", vpc_id,
                                          "ID of VPC to create resources in"))
-    
+
     external_subnet_id = lib.subnet_id_lookup(session, "external." + domain)
     config.add_arg(configuration.Arg.Subnet("ExternalSubnet",
                                             external_subnet_id,
                                             "ID of External Subnet to create resources in"))
-                                            
+
     internal_sg_id = lib.sg_lookup(session, vpc_id, "internal." + domain)
     config.add_arg(configuration.Arg.SecurityGroup("InternalSecurityGroup",
                                                    internal_sg_id,
                                                    "ID of internal Security Group"))
-    
+
     # Create the a and b subnets
     config.subnet_domain = "a." + domain
     config.subnet_subnet = hosts.lookup(config.subnet_domain)
     config.add_subnet("ASubnet", az="us-east-1b")
-    
+
     config.subnet_domain = "b." + domain
     config.subnet_subnet = hosts.lookup(config.subnet_domain)
     config.add_subnet("BSubnet", az="us-east-1c") # BSubnet needs to be in a different AZ from ASubnet
-    
+
     # Dynamically add the RDS instance address to the user data, so that
     # the endpoint server can access the launched DB
     # Fn::GetAtt and Fn::Join are CloudFormation template functions
     user_data_dynamic = [user_data, "\n[aws]\n",
                          "db = ", { "Fn::GetAtt" : [ "DB", "Endpoint.Address" ] }, "\n"]
     user_data_ = { "Fn::Join" : ["", user_data_dynamic]}
-    
+
     config.add_ec2_instance("Endpoint",
                             "endpoint.external." + domain,
                             lib.ami_lookup(session, "endpoint.boss"),
@@ -94,20 +94,20 @@ def create_config(session, domain, keypair=None, user_data=None, db_config={}):
                                 ("tcp", "80", "80", "0.0.0.0/0"),
                                 ("tcp", "443", "443", "0.0.0.0/0")
                               ])
-    
+
     return config
-  
+
 def generate(folder, domain):
     """Create the configuration and save it to disk"""
     name = lib.domain_to_stackname("production." + domain)
     config = create_config(None, domain)
     config.generate(name, folder)
-                          
+
 def create(session, domain):
     """Configure Vault, create the configuration, and launch it"""
     keypair = lib.keypair_lookup(session)
-    
-    def call_vault(command, *args, **kawargs):
+
+    def call_vault(command, *args, **kwargs):
         """A wrapper function around lib.call_vault() that populates most of
         the needed arguments."""
         return lib.call_vault(session,
@@ -115,14 +115,14 @@ def create(session, domain):
                               "bastion." + domain,
                               "vault." + domain,
                               command, *args, **kwargs)
-    
+
     db = {
         "name":"boss",
         "user":"testuser",
         "password": lib.generate_password(),
         "port": "3306"
     }
-    
+
     # Configure Vault and create the user data config that the endpoint will
     # use for connecting to Vault and the DB instance
     endpoint_token = call_vault("vault-provision", "endpoint")
@@ -134,14 +134,14 @@ def create(session, domain):
     user_data["system"]["fqdn"] = "endpoint.external." + domain
     user_data["system"]["type"] = "endpoint"
     user_data = str(user_data)
-    
+
     # Should transition from vault-django to vault-write
     call_vault("vault-django", db["name"], db["user"], db["password"], db["port"])
 
     try:
         name = lib.domain_to_stackname("production." + domain)
         config = create_config(session, domain, keypair, user_data, db)
-        
+
         success = config.create(session, name)
         if not success:
             raise Exception("Create Failed")
--- vault/vault.py
+++ vault/vault.py
@@ -28,47 +28,47 @@ _CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 def get_path(machine, filename):
     if machine is None:
         machine = ""
-        
+
     path = os.path.join(_CURRENT_DIR, "private", machine, filename)
-    
+
     os.makedirs(os.path.dirname(path), exist_ok = True)
-    
+
     return path
 
 def get_client(read_token = None, machine = None):
     """Open a connection to the Vault located at http://localhost:8200
-    
+
     If read_token == True then read the token from VAULT_TOKEN and verify
     that the client is authenticated to the Vault.
-    
+
     Exits (sys.exit) if read_token and VAULT_TOKEN does not exists
     Exits (sys.exit) if read_token and VAULT_TOKEN does not contain a valid
                      token
     """
     client = hvac.Client(url="http://localhost:8200")
-    
+
     if read_token is not None:
         token_file = get_path(machine, read_token)
         if not os.path.exists(token_file):
             print("Need the root token to communicate with the Vault, exiting...")
             sys.exit(1)
-            
+
         with open(token_file, "r") as fh:
             client.token = fh.read()
             if not client.is_authenticated():
                 print("Vault token is not valid, cannot communicate with the Vault, exiting...")
                 sys.exit(1)
-            
+
     return client
 
 def vault_init(machine = None, secrets = 5, threashold = 3):
     """Initialize a Vault. Connect using get_client() and if the Vault is not
     initialized then initialize it with 5 secrets and a threashold of 3. The
     keys are stored as VAULT_KEY and root token is stored as VAULT_TOKEN.
-    
+
     After initializing the Vault it is unsealed for use.
     """
-       
+
     client = get_client(machine = machine)
     if client.is_initialized():
         print("Vault is already initialized")
@@ -76,7 +76,7 @@ def vault_init(machine = None, secrets = 5, threashold = 3):
 
     print("Initializing with {} secrets and {} needed to unseal".format(secrets, threashold))
     result = client.initialize(secrets, threashold)
-    
+
     token_file = get_path(machine, VAULT_TOKEN)
     key_file = get_path(machine, VAULT_KEY)
     with open(token_file, "w") as fh:
@@ -84,21 +84,21 @@ def vault_init(machine = None, secrets = 5, threashold = 3):
     for i in range(secrets):
         with open(key_file + str(i+1), "w") as fh:
             fh.write(result["keys"][i])
-       
+
     print()
     print("======== WARNING WARNING WARNING ========")
     print("= Vault root token and unseal keys were =")
     print("= written to disk. PROTECT these files. =")
     print("======== WARNING WARNING WARNING ========")
-    
+
     print()
     print("Unsealing Vault")
     client.unseal_multi(result["keys"])
-    
+
     print()
     print("Configuring Vault")
     vault_configure(machine)
-    
+
 def vault_configure(machine = None):
     """A companion function that will configure a newly initialized Vault
     as needed for BOSS. This includes:
@@ -113,13 +113,13 @@ def vault_configure(machine = None):
         * Configure PKI backend roles from policies/*.pki
     """
     client = get_client(read_token = VAULT_TOKEN, machine = machine)
-    
+
     # Audit Backend
     audit_options = {
         'low_raw': 'True',
     }
     client.enable_audit_backend('syslog', options=audit_options)
-    
+
     # Policies
     provisioner_policies = []
     path = os.path.join(_CURRENT_DIR, "policies", "*.hcl")
@@ -127,16 +127,16 @@ def vault_configure(machine = None):
         name = os.path.basename(policy).split('.')[0]
         with open(policy, 'r') as fh:
             client.set_policy(name, fh.read())
-            
+
         # Add every policy to the provisioner, as it has to have the
         # superset of any policies that it will provision
         provisioner_policies.append(name)
-        
+
     token_file = get_path(machine, PROVISIONER_TOKEN)
     token = client.create_token(policies=provisioner_policies)
     with open(token_file, "w") as fh:
         fh.write(token['auth']['client_token'])
-    
+
     # AWS Backend
     vault_aws_creds = os.path.join(_CURRENT_DIR, "private", "vault_aws_credentials")
     if not os.path.exists(vault_aws_creds):
@@ -156,7 +156,7 @@ def vault_configure(machine = None):
                 client.write("aws/roles/" + name, policy = fh.read())
 
     # PKI Backend
-    if False: # Disabled until we either have a CA cert or can generate a CA
+    if True: # Disabled until we either have a CA cert or can generate a CA
         print("Vault PKI cert file does not exist, skipping...")
     else:
         client.enable_secret_backend('pki')
@@ -165,25 +165,25 @@ def vault_configure(machine = None):
         response = client.write("pki/root/generate/internal", common_name="boss.io")
         with open(get_path(machine, "ca.pem"), 'w') as fh:
             fh.write(response["data"]["certificate"])
-        
+
         # Should we configure CRL?
-        
+
         path = os.path.join(_CURRENT_DIR, "policies", "*.pki")
         for pki in glob.glob(path):
             name = os.path.basename(pki).split('.')[0]
             with open(pki, 'r') as fh:
                 keys = json.load(fh)
                 client.write("aws/roles/" + name, **keys)
-    
+
 def vault_shell(machine = None):
     """Create a connection to Vault and then drop the user into an interactive
     shell (just like the python interperter) with 'client' holding the Vault
     connection object.
     """
     import code
-    
+
     client = get_client(read_token = VAULT_TOKEN, machine = machine)
-    
+
     code.interact(local=locals())
 
 def verify(machine = None):
@@ -191,19 +191,19 @@ def verify(machine = None):
     to Vault.
     """
     client = get_client(read_token = VAULT_TOKEN, machine = machine)
-    
+
     token_file = get_path(machine, PROVISIONER_TOKEN)
     policy = "provisioner"
-    
+
     if False:
         pprint(client.list_policies())
         pprint(client.read("/sys/policy/" + policy))
-    
+
     if True:
         client.delete_policy(policy)
         with open("policies/{}.hcl".format(policy), "r") as fh:
             client.set_policy(policy, fh.read())
-            
+
     if True:
         with open(token_file, 'r') as fh:
             result = client.revoke_token(fh.read())
@@ -211,36 +211,36 @@ def verify(machine = None):
             result = client.create_token(policies=["provisioner","endpoint"])
             pprint(result)
             fh.write(result['auth']['client_token'])
-            
+
     if False:
         with open(token_file, 'r') as fh:
             result = client.lookup_token(fh.read())
             pprint(result)
-    
+
 def vault_unseal(machine = None):
     """Unseal a sealed Vault. Connect using get_client() and if the Vault is
     not sealed read all of the keys defined by VAULT_KEY and unseal.
-    
+
     If there are not enough keys to completely unseal the Vault, print a
     status message about how many more keys are required to finish the
     process.
     """
-    
+
     client = get_client(machine = machine)
     if not client.is_sealed():
         print("Vault is already unsealed")
         return
-        
+
     key_file = get_path(machine, VAULT_KEY)
     keys = []
     for f in glob.glob(key_file + "*"):
         with open(f, "r") as fh:
             keys.append(fh.read())
-            
+
     if len(keys) == 0:
         print("Could not locate any key files, not unsealing")
         return
-    
+
     res = client.unseal_multi(keys)
     if res['sealed']:
         p = res['progress']
@@ -249,15 +249,15 @@ def vault_unseal(machine = None):
         print("Enter {} more keys to finish unsealing the vault". format(t-p))
     else:
         print("Vault unsealed")
-        
+
 def vault_seal(machine = None):
     """Seal an unsealed Vault. Connect using get_client(True) and if the Vault
     is unsealed, seal it.
-    
+
     Used to quickly protect a Vault without having to stop the Vault service
     on a protected VM.
     """
-    
+
     client = get_client(read_token = VAULT_TOKEN, machine = machine)
     if client.is_sealed():
         print("Vault is already sealed")
@@ -265,7 +265,7 @@ def vault_seal(machine = None):
 
     client.seal()
     print("Vault is sealed")
-    
+
 def vault_status(machine = None):
     """Print the status of a Vault. Connect using get_client(True) and print
     the status of the following items (if available):
@@ -278,60 +278,60 @@ def vault_status(machine = None):
      * Audit backends
      * Auth backends
     """
-    
+
     client = get_client(machine = machine)
     if not client.is_initialized():
         print("Vault is not initialized")
         return
     else:
         print("Vault is initialized")
-    
+
     if client.is_sealed():
         print("Vault is sealed")
         print(client.seal_status)
         return
     else:
         print("Vault is unsealed")
-    
+
     # read in the Vault access token
     client = get_client(read_token = VAULT_TOKEN, machine = machine)
     print()
     print("Key Status")
     print(json.dumps(client.key_status))
-    
+
     print()
     print("HA Status")
     print(json.dumps(client.ha_status))
-    
+
     print()
     print("Secret Backends")
     print(json.dumps(client.list_secret_backends(), indent=True))
-    
+
     print()
     print("Policies")
     print(json.dumps(client.list_policies()))
-    
+
     print()
     print("Audit Backends")
     print(json.dumps(client.list_audit_backends(), indent=True))
-    
+
     print()
     print("Auth Backends")
     print(json.dumps(client.list_auth_backends(), indent=True))
-    
+
 def vault_provision(policy, machine = None):
     """Create a new Vault access token. Connect using get_client(True),
     request a new access token (default policy), and save it to NEW_TOKEN.
     """
     client = get_client(read_token = PROVISIONER_TOKEN, machine = machine)
-    
+
     token = client.create_token(policies = [policy])
     return token["auth"]["client_token"]
-        
+
 def _vault_provision(machine = None):
     policy = input("policy: ")
     token = vault_provision(policy, machine)
-    
+
     token_file = get_path(machine, NEW_TOKEN)
     with open(token_file, "w") as fh:
         fh.write(token)
@@ -341,13 +341,13 @@ def vault_revoke(token, machine = None):
     token (using input()), and then revoke the token.
     """
     client = get_client(read_token = PROVISIONER_TOKEN, machine = machine)
-    
+
     client.revoke_token(token)
-    
+
 def _vault_revoke(machine = None):
     token = input("token: ") # prompt for token or ready fron NEW_TOKEN (or REVOKE_TOKEN)?
     vault_revoke(token, machine)
-    
+
 def vault_django(db_name, username, password, port, machine = None):
     """Provision a Vault with credentials for a Django webserver. Connect
     using get_client(True) and provision the following information:
@@ -364,7 +364,7 @@ def vault_django(db_name, username, password, port, machine = None):
        - 'secret/endpoint/django/db port='
     """
     client = get_client(read_token = PROVISIONER_TOKEN, machine = machine)
-    
+
     client.write("secret/endpoint/django", secret_key = str(uuid.uuid4()))
     db = {
         "name": db_name,
@@ -373,12 +373,12 @@ def vault_django(db_name, username, password, port, machine = None):
         "port": port
     }
     client.write("secret/endpoint/django/db", **db)
-        
+
 def _vault_django(machine = None):
     args = []
     for key in ["name", "user", "password", "port"]:
         args.append(input(key + ": "))
-        
+
     vault_django(*args, machine = machine)
 
 # Should probably create a _vault_write() function that reads
@@ -389,7 +389,7 @@ def vault_write(path, machine = None, **kwargs):
     """
     client = get_client(read_token = PROVISIONER_TOKEN, machine = machine)
     client.write(path, kwargs)
-   
+
 COMMANDS = {
     "vault-init": vault_init,
     "vault-configure": vault_configure,
