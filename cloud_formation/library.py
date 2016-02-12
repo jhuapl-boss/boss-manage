077ee75ac8fefdc40e81d0866e8c9a60a56f67fd
Tim Gion <timothy.gion@jhuapl.edu>
2016-02-12 17:07:04 -0500

    Added function to lookup AWS instance ID by name.

--- cloud_formation/library.py
+++ cloud_formation/library.py
@@ -26,7 +26,7 @@ def domain_to_stackname(domain):
 def template_argument(key, value, use_previous = False):
     """Create a JSON dictionary formated as a CloudFlormation template
     argument.
-    
+
     use_previous is passed as UserPreviousValue to CloudFlormation.
     """
     return {"ParameterKey": key, "ParameterValue": value, "UsePreviousValue": use_previous}
@@ -38,7 +38,7 @@ def keypair_to_file(keypair):
         print("Error: SSH Key '{}' does not exist".format(file))
         return None
     return file
- 
+
 def call_vault(session, bastion_key, bastion_host, vault_host, command, *args, **kwargs):
     """Call ../vault/bastion.py with a list of hardcoded AWS / SSH arguments.
     This is a common function for any other function that needs to populate
@@ -50,9 +50,9 @@ def call_vault(session, bastion_key, bastion_host, vault_host, command, *args, *
         # Have to dynamically lookup the function because vault.COMMANDS
         # references the command line version of the commands we want to execute
         return vault.__dict__[command.replace('-','_')](*args, machine=vault_host, **kwargs)
-        
+
     return bastion.connect_vault(bastion_key, vault_ip, bastion_ip, cmd)
- 
+
 def password(what):
     """Prompt the user for a password and verify it."""
     while True:
@@ -62,16 +62,16 @@ def password(what):
             return pass_
         else:
             print("Passwords didn't match, try again.")
-            
+
 def generate_password(length = 16):
     """Generate an alphanumeric password of the given length."""
     chars = string.ascii_letters + string.digits #+ string.punctuation
     return "".join([chars[c % len(chars)] for c in os.urandom(length)])
- 
+
 def vpc_id_lookup(session, vpc_domain):
     """Lookup the Id for the VPC with the given domain name."""
     if session is None: return None
-    
+
     client = session.client('ec2')
     response = client.describe_vpcs(Filters=[{"Name":"tag:Name", "Values":[vpc_domain]}])
     if len(response['Vpcs']) == 0:
@@ -82,51 +82,51 @@ def vpc_id_lookup(session, vpc_domain):
 def subnet_id_lookup(session, subnet_domain):
     """Lookup the Id for the Subnet with the given domain name."""
     if session is None: return None
-    
+
     client = session.client('ec2')
     response = client.describe_subnets(Filters=[{"Name":"tag:Name", "Values":[subnet_domain]}])
     if len(response['Subnets']) == 0:
         return None
     else:
         return response['Subnets'][0]['SubnetId']
-        
+
 def ami_lookup(session, ami_name):
     """Lookup the Id for the AMI with the given name."""
     if session is None: return None
-    
+
     client = session.client('ec2')
     response = client.describe_images(Filters=[{"Name":"name", "Values":[ami_name]}])
     if len(response['Images']) == 0:
         return None
     else:
         return response['Images'][0]['ImageId']
-        
+
 def sg_lookup(session, vpc_id, group_name):
     """Lookup the Id for the VPC Security Group with the given name."""
     if session is None: return None
-    
+
     client = session.client('ec2')
     response = client.describe_security_groups(Filters=[{"Name":"vpc-id", "Values":[vpc_id]},
                                                         {"Name":"tag:Name", "Values":[group_name]}])
-                                                        
+
     if len(response['SecurityGroups']) == 0:
         return None
     else:
         return response['SecurityGroups'][0]['GroupId']
-        
+
 def rt_lookup(session, vpc_id, rt_name):
     """Lookup the Id for the VPC Route Table with the given name."""
     if session is None: return None
-    
+
     client = session.client('ec2')
     response = client.describe_route_tables(Filters=[{"Name":"vpc-id", "Values":[vpc_id]},
                                                      {"Name":"tag:Name", "Values":[rt_name]}])
-                                                     
+
     if len(response['RouteTables']) == 0:
         return None
     else:
         return response['RouteTables'][0]['RouteTableId']
-        
+
 def rt_name_default(session, vpc_id, new_rt_name):
     """Find the default VPC Route Table and give it a name so that it can be referenced latter.
     Needed because by default the Route Table does not have a name and rt_lookup() will not find it. """
@@ -138,11 +138,11 @@ def rt_name_default(session, vpc_id, new_rt_name):
     resource = session.resource('ec2')
     rt = resource.RouteTable(rt_id)
     response = rt.create_tags(Tags=[{"Key": "Name", "Value": new_rt_name}])
-        
+
 def peering_lookup(session, from_id, to_id):
     """Lookup the Id for the Peering Connection between the two VPCs."""
     if session is None: return None
-    
+
     client = session.client('ec2')
     response = client.describe_vpc_peering_connections(Filters=[{"Name":"requester-vpc-info.vpc-id", "Values":[from_id]},
                                                                 {"Name":"requester-vpc-info.owner-id", "Values":["256215146792"]},
@@ -150,16 +150,16 @@ def peering_lookup(session, from_id, to_id):
                                                                 {"Name":"accepter-vpc-info.owner-id", "Values":["256215146792"]},
                                                                 {"Name":"status-code", "Values":["active"]},
                                                                 ])
-                                                                
+
     if len(response['VpcPeeringConnections']) == 0:
         return None
     else:
         return response['VpcPeeringConnections'][0]['VpcPeeringConnectionId']
-        
+
 def keypair_lookup(session):
     """Print the valid key pairs for the session and ask the user which to use."""
     if session is None: return None
-    
+
     client = session.client('ec2')
     response = client.describe_key_pairs()
     print("Key Pairs")
@@ -174,4 +174,25 @@ def keypair_lookup(session):
             return response['KeyPairs'][idx]['KeyName']
         except:
             print("Invalid Key Pair number, try again")
-            
+
+
+def instanceid_lookup(session, hostname):
+    """Look up instance id by hostname."""
+    if session is None: return None
+
+    client = session.client('ec2')
+    response = client.describe_instances(
+        Filters=[{"Name":"tag:Name", "Values":[hostname]}])
+
+    item = response['Reservations']
+    if len(item) == 0:
+        return None
+    else:
+        item = item[0]['Instances']
+        if len(item) == 0:
+            return None
+        else:
+            item = item[0]
+            if 'InstanceId' in item:
+                return item['InstanceId']
+            return None
