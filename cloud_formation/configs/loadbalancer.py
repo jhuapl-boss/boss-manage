765a86c0a84502783711f8e3309752c22a1d0a1e
Derek Pryor <Derek.Pryor@jhuapl.edu>
2016-03-30 14:38:00 -0400

    Updated loadbalancer to correctly configure auth urls

--- cloud_formation/configs/loadbalancer.py
+++ cloud_formation/configs/loadbalancer.py
@@ -98,9 +98,20 @@ def create(session, domain):
         print("KeyPair to communicating with Vault")
         keypair = lib.keypair_lookup(session)
         call = lib.ExternalCalls(session, keypair, domain)
-        dns = lib.elb_public_lookup(session, "elb." + domain)
-        uri = "http://{}".format(dns)
-        call.vault_update("secret/endpoint/auth", public_uri = uri)
+
+        def configure_auth(auth_port):
+            dns = lib.elb_public_lookup(session, "elb." + domain)
+            uri = "http://{}".format(dns)
+            call.vault_update("secret/endpoint/auth", public_uri = uri)
+
+            creds = call.vault_read("secret/auth")
+            kc = lib.KeyCloakClient("http://localhost:{}".format(auth_port))
+            kc.login(creds["username"], creds["password"])
+            kc.add_redirect_uri("BOSS","endpoint", uri + "/*")
+            kc.logout()
+        call.set_ssh_target("auth")
+        call.ssh_tunnel(configure_auth, 8080)
+
         print("Restart Django on the Endpoint Servers")
     else:
         print('failed')
