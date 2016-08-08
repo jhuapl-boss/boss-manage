f142b3c317d7ef7b5bc0f7720ace1a8bc788a790
Tim Gion <timothy.gion@jhuapl.edu>
2016-08-08 10:32:41 -0400

    Added name of Dynamo s3 index table to boss.config.

--- cloud_formation/configs/production.py
+++ cloud_formation/configs/production.py
@@ -250,6 +250,7 @@ def create(session, domain):
     user_data["aws"]["s3-flush-queue"] = '{{"Ref": "{}" }}'.format(s3flushqname)
     user_data["aws"]["s3-flush-deadletter-queue"] = '{{"Ref": "{}" }}'.format(deadqname)
     user_data["aws"]["cuboid_bucket"] = "cuboids." + domain
+    user_data["aws"]["s3-index-table"] = "s3index." + domain
     user_data["auth"]["OIDC_VERIFY_SSL"] = str(domain in hosts.BASE_DOMAIN_CERTS.keys())
 
     call.vault_write(VAULT_DJANGO, secret_key = str(uuid.uuid4()))
