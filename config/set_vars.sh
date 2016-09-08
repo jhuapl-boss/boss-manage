e360911525529b3d360c7b216d2d5e897c95516b
Priya Manavalan <pmanava1@jhu.edu>
2016-09-08 15:07:41 -0400

    Updated submodule references

--- config/set_vars.sh
+++ config/set_vars.sh
@@ -1,5 +1,6 @@
 export AWS_CREDENTIALS=../config/aws-credentials
-export SSH_KEY=~/.ssh/test.boss.pem
+export SSH_KEY=~/.ssh/theboss.io-20160518.pem
+#export SSH_KEY=~/.ssh/microns-bastion20151117.pem
 export BASTION_KEY=~/.ssh/microns-bastion20151117.pem
 export BASTION_IP=52.3.13.189
 export BASTION_USER=ubuntu
