da422ce7a7142ab4e39cb075d63d25cb24def80b
Sandy Hider <sandy.hider@jhuapl.edu>
2017-01-12 18:56:43 -0500

    added missing import

--- cloud_formation/configs/activities.py
+++ cloud_formation/configs/activities.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from lib.cloudformation import CloudFormationConfiguration, Ref, Arn, get_scenario
+from lib.cloudformation import CloudFormationConfiguration, Ref, Arn, get_scenario, Arg
 from lib.userdata import UserData
 from lib.names import AWSNames
 from lib import aws
