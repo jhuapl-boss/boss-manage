e55db4c99ea8e1df99b619d23020b4d99ecea552
Derek Pryor <Derek.Pryor@jhuapl.edu>
2017-01-12 14:45:50 -0800

    Added argparse support to bin/stepfunctions.py

--- bin/stepfunctions.py
+++ bin/stepfunctions.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 
+# Copyright 2016 The Johns Hopkins University Applied Physics Laboratory
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
 import sys
 from pathlib import Path
 
@@ -7,8 +23,29 @@ import alter_path
 from lib.stepfunctions import heaviside
 
 if __name__ == '__main__':
-    file_name = sys.argv[1]
+    parser = argparse.ArgumentParser(description = "Script to compile a heaviside file into a StepFunction State Machine",
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--output", "-o",
+                        metavar = "<file>",
+                        default = sys.stdout,
+                        type = argparse.FileType('w'),
+                        help = "Location to save the StepFunction State Machine to (default: stdout)")
+    parser.add_argument("--region", "-r",
+                        metavar = "<aws_region>",
+                        default = '',
+                        help = "AWS Region for ARNs (default: '')")
+    parser.add_argument("--account", "-a",
+                        metavar = "<aws_account>",
+                        default = '',
+                        help = "AWS Account ID for ARNs (default: '')")
+    parser.add_argument("file",
+                        help="heaviside file to compile")
 
-    def_ = heaviside.compile(Path(file_name), region='', account_id='', indent=3)
+    args = parser.parse_args()
 
-    print(def_)
+    machine = heaviside.compile(Path(args.file),
+                                region=args.region,
+                                account_id=args.account,
+                                indent=3)
+
+    args.output.write(machine)
