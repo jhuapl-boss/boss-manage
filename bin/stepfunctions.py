783c5ab9f0ec80735ba36a0c51a0e87b0e5f7e38
Derek Pryor <Derek.Pryor@jhuapl.edu>
2017-01-12 13:46:36 -0800

    Fixed stepfunction script for compiling a DSL file

--- bin/stepfunctions.py
+++ bin/stepfunctions.py
@@ -4,11 +4,11 @@ import sys
 from pathlib import Path
 
 import alter_path
-from lib.stepfunctions import compile
+from lib.stepfunctions import heaviside
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
 
-    def_ = compile(Path(file_name), region='', account_id='', indent=3)
+    def_ = heaviside.compile(Path(file_name), region='', account_id='', indent=3)
 
     print(def_)
