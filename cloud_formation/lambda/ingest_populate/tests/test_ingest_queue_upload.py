4421420a1f3ee45ce1204c3cf9a9dba0d4c61069
Sandy Hider <sandy.hider@jhuapl.edu>
2018-03-28 13:50:44 -0400

    lastest changes - still working them

--- cloud_formation/lambda/ingest_populate/tests/test_ingest_queue_upload.py
+++ cloud_formation/lambda/ingest_populate/tests/test_ingest_queue_upload.py
@@ -29,48 +29,101 @@ import ingestclient.core.backend as backend
 @patch('boto3.resource')
 class TestIngestQueueUploadLambda(unittest.TestCase):
 
-    def test_something(self, fake_resource):
+    # def test_something(self, fake_resource):
+    #
+    #     # This puts in a mock for boto3.resource('sqs').Queue().
+    #     queue = MagicMock()
+    #     #fake_resource.return_value = queue
+    #     queue.send_messages.return_value = {'Successful': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
+    #     fake_resource.return_value.Queue.return_value = queue
+    #     context = None
+    #     args = {
+    #         "upload_sfn": "IngestUpload",
+    #         "x_start": 0,
+    #         "x_stop": 2048,
+    #         "y_start": 0,
+    #         "y_stop": 2048,
+    #         "z_start": 0,
+    #         "z_stop": 20,
+    #         "t_start": 0,
+    #         "t_stop": 1,
+    #         "project_info": [
+    #           "3",
+    #           "3",
+    #           "3"
+    #         ],
+    #         "ingest_queue": "https://queue.amazonaws.com/...",
+    #         "job_id": 11,
+    #         "upload_queue": "https://queue.amazonaws.com/...",
+    #         "x_tile_size": 1024,
+    #         "y_tile_size": 1024,
+    #         "t_tile_size": 1,
+    #         "z_tile_size": 1,
+    #         "resolution": 0,
+    #         "tiles_to_skip": 0,
+    #         'MAX_NUM_TILES_PER_LAMBDA': 30000,
+    #         'z_chunk_size': 16
+    #
+    #     }
+    #
+    #     actual = iqu.handler(args, context)
+    #     self.assertEqual(80, actual)
+    #
+    #
+    # def test_create_messages(self, fake_resource):
+    #
+    #     # This puts in a mock for boto3.resource('sqs').Queue().
+    #     #queue = MagicMock()
+    #     #fake_resource.return_value = queue
+    #     context = None
+    #     args = {
+    #         "upload_sfn": "IngestUpload",
+    #         "x_start": 0,
+    #         "x_stop": 2048,
+    #         "y_start": 0,
+    #         "y_stop": 2048,
+    #         "z_start": 0,
+    #         "z_stop": 20,
+    #         "t_start": 0,
+    #         "t_stop": 1,
+    #         "project_info": [
+    #           "3",
+    #           "3",
+    #           "3"
+    #         ],
+    #         "ingest_queue": "https://queue.amazonaws.com/...",
+    #         "job_id": 11,
+    #         "upload_queue": "https://queue.amazonaws.com/...",
+    #         "x_tile_size": 1024,
+    #         "y_tile_size": 1024,
+    #         "t_tile_size": 1,
+    #         "z_tile_size": 1,
+    #         "resolution": 0,
+    #         "tiles_to_skip": 3,
+    #         'MAX_NUM_TILES_PER_LAMBDA': 40,
+    #         'z_chunk_size': 16
+    #
+    #     }
+    #
+    #     msg = iqu.create_messages(args)
+    #     for i in range(45):
+    #         try:
+    #             print(i)
+    #             args_ = next(msg)
+    #             print(str(args_))
+    #             pprint.pprint(args_)
+    #         except StopIteration:
+    #             break_count = i;
+    #             break
+    #     print("breakcount = {}".format(break_count))
+    #     self.assertEqual(40, break_count)
 
-        # This puts in a mock for boto3.resource('sqs').Queue().
-        queue = MagicMock()
-        #fake_resource.return_value = queue
-        queue.send_messages.return_value = {'Successful': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
-        fake_resource.return_value.Queue.return_value = queue
-        context = None
-        args = {
-            "upload_sfn": "IngestUpload",
-            "x_start": 0,
-            "x_stop": 2048,
-            "y_start": 0,
-            "y_stop": 2048,
-            "z_start": 0,
-            "z_stop": 20,
-            "t_start": 0,
-            "t_stop": 1,
-            "project_info": [
-              "3",
-              "3",
-              "3"
-            ],
-            "ingest_queue": "https://queue.amazonaws.com/...",
-            "job_id": 11,
-            "upload_queue": "https://queue.amazonaws.com/...",
-            "x_tile_size": 1024,
-            "y_tile_size": 1024,
-            "t_tile_size": 1,
-            "z_tile_size": 1,
-            "resolution": 0,
-            "tiles_to_skip": 0,
-            'MAX_NUM_TILES_PER_LAMBDA': 30000,
-            'z_chunk_size': 16
 
-        }
-
-        actual = iqu.handler(args, context)
-        self.assertEqual(80, actual)
-
-
-    def test_create_messages(self, fake_resource):
+    def test_messages_lists(self, fake_resource):
+        # create 3 Tests first pulls down entire list of
+        # second skips X
+        # 3rd skips x more
+        # compare the list with the full list.
 
         # This puts in a mock for boto3.resource('sqs').Queue().
         #queue = MagicMock()
@@ -100,10 +153,19 @@ class TestIngestQueueUploadLambda(unittest.TestCase):
             "z_tile_size": 1,
             "resolution": 0,
             "tiles_to_skip": 3,
-            'MAX_NUM_TILES_PER_LAMBDA': 40,
+            'MAX_NUM_TILES_PER_LAMBDA': 500000,
             'z_chunk_size': 16
 
         }
+
+        msgs = iqu.create_messages(args)
+        for msg in msgs:
+            msg_json = json.loads(msg)
+            key_parts = be.decode_tile_key(msg_json['tile_key'])
+            cat_maid.process(key_parts["x_index"],
+                             key_parts["y_index"],
+                             key_parts["z_index"],
+                             key_parts["t_index"])
 
         msg = iqu.create_messages(args)
         for i in range(45):
@@ -117,6 +179,7 @@ class TestIngestQueueUploadLambda(unittest.TestCase):
                 break
         print("breakcount = {}".format(break_count))
         self.assertEqual(40, break_count)
+
 
 
     # Test for Catmaid Need to be moved to a separate test file.
