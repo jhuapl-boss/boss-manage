73f5edae3fa023e37c180fb2b3d6911eeac07b30
Tim Gion <movestill@gmail.com>
2018-04-23 17:58:41 -0400

    Update all lambda functions that use multilambda.
    A list of lambda functions that use multilambda is maintained inside
    update_lambda_code().

--- bin/update_lambda_fcn.py
+++ bin/update_lambda_fcn.py
@@ -35,6 +35,7 @@ from lib import constants as const
 from lib import zip
 
 import argparse
+import botocore
 import configparser
 import os
 import sys
@@ -81,12 +82,16 @@ def update_lambda_code(session, domain, bucket):
         names.downsample_volume_lambda
     ]
     client = session.client('lambda')
-    resp = client.update_function_code(
-        FunctionName=names.multi_lambda,
-        S3Bucket=bucket,
-        S3Key=get_lambda_zip_name(domain),
-        Publish=True)
-    print(resp)
+    for lambda_name in uses_multilambda:
+        try:
+            resp = client.update_function_code(
+                FunctionName=lambda_name,
+                S3Bucket=bucket,
+                S3Key=get_lambda_zip_name(domain),
+                Publish=True)
+            print(resp)
+        except botocore.exceptions.ClientError as ex:
+            print('Error updating {}: {}'.format(lambda_name, ex))
 
 # DP TODO: Move to a lib/ library
 def load_lambdas_on_s3(session, domain, bucket):
