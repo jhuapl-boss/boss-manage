d5987911163a7da6f0d4c28a4ecfd5ab93977503
lrodri29 <lrodri729@gmail.com>
2018-08-15 16:58:54 -0400

    print test

--- lib/cloudformation.py
+++ lib/cloudformation.py
@@ -466,6 +466,9 @@ class CloudFormationConfiguration:
 
         client = session.client('cloudformation')
         print(utils.get_commit())
+        print(self.stack_name)
+        print(self._create_template())
+        print(self.arguments)
         try:
             response = client.create_stack(
                 StackName = self.stack_name,
