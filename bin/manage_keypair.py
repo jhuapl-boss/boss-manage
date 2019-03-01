1dcd68ace74fb17614891646da77d836e7e435c1
lrodri29 <lrodri729@gmail.com>
2019-03-01 18:13:40 -0500

    Changes to manage_keypair

--- bin/manage_keypair.py
+++ bin/manage_keypair.py
@@ -21,15 +21,10 @@ Was written primarily to run from within an ec2 instance."""
 import argparse
 import sys
 import os
-import subprocess
 
 import alter_path
 from lib import aws
 from pathlib import Path
-
-#Define file pathname
-cur_dir = os.path.dirname(os.path.realpath(__file__))
-home = str(Path.home())
 
 if __name__ == '__main__':
 
@@ -56,41 +51,52 @@ if __name__ == '__main__':
     parser.add_argument("keypairName",
                         metavar = "keypairName",
                         help = "Name of keypair to manage")
-    parser.add_argument("--internal",
-                        action = "store_true",
-                        help="Attemps to execute cloudformation script without any credentials. Meant to use from internal aws instances")
     args = parser.parse_args()
 
-    if args.internal:
-        session = aws.use_iam_role()
-
-    else:
-        if args.aws_credentials is None:
+    if args.aws_credentials is None:
+        try:
+            print("AWS credentials not provided and AWS_CREDENTIALS is not defined, assuming IAM role")
+            session = aws.use_iam_role()
+        except Exception as e:
             parser.print_usage()
-            print("Error: AWS credentials not provided and AWS_CREDENTIALS is not defined")
-            sys.exit(1)
+            print('Error: Could not assume IAM role')
+    else:
         session = aws.create_session(args.aws_credentials)
 
     client = session.client('ec2')
 
+    #Define key pair path
+    key_file_path = Path(str(Path.home()) + '/.ssh/' + str(args.keypairName) + '.pem')
+
     if args.action == 'create':
-        response = aws.create_keypair(session, args.keypairName)
-        subprocess.call('mkdir ' + home + '/.ssh',shell=True)
-        pem_file = open(home + '/.ssh/' + args.keypairName + '.pem','w')
-        pem_file.write(response['KeyMaterial'])
-        pem_file.close()
-        print('KeyPair saved in ' + '~/.ssh/')
-        print('Protect this keypair and make sure you have access to it.')
+        try:
+            response = aws.create_keypair(session, args.keypairName)
+            print('Protect this keypair and make sure you have access to it.')
+        except Exception as e:
+            print('Failed to create keypair due to: {}'.format(e))
+            response = False
+
+        if response:   
+            try:
+                key_file_path.touch()
+                key_file_path.open('w').write(response['KeyMaterial'])
+                print('KeyPair saved in ' + '~/.ssh/')
+            except FileExistsError:
+                print('Directory {} already existed'.format(key_dir))
+                pass
 
     elif args.action == 'delete':
         response = aws.delete_keypair(session, args.keypairName)
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
             try:
-                subprocess.call('rm ' + home + '/.ssh/' + args.keypairName + '.pem', shell=True)
+                 os.remove(str(key_file_path))
+                 print('KeyPair deleted successfully')
             except NameError:
                 print('The keypair was deleted from aws but it was not in your .ssh/ directory')
                 pass
-            print('KeyPair deleted successfully')
+            except FileNotFoundError:
+                print('Could not find the PEM key to delete under ' + key_file_path)
+                pass
         else:
             print(response['ResponseMetadata'])
 
