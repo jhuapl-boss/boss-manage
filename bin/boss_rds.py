f4e15ffbae9e28222821949a364fbf447530b4e8
Luis <luis.rodriguez@jhuapl.edu>
2019-06-03 11:24:32 -0400

    Move logic into lib/boss_rds.py

--- bin/boss_rds.py
+++ bin/boss_rds.py
@@ -21,7 +21,6 @@ COMMANDS : A dictionary of available commands and the functions to call
 
 import argparse
 import sys, os
-
 import boto3
 import logging
 import alter_path
@@ -29,134 +28,10 @@ import alter_path
 from lib import aws
 from lib import boss_rds
 
-def sql_list(session, domain, db_table):
-    """
-    List all the available members of a given sql table.
-
-    Args:
-        session (Session): Open boto3 session.
-        domain (str): VPC such as integration.boss.
-        db_table: Identifies which table members to list.
-
-    Returns:
-        (str): Lookup key.
-    """
-    query = "SELECT * FROM {}".format(db_table)
-
-    logging.info('Tunneling to DB (slow) . . .')
-    with boss_rds.connect_rds(session, domain) as sql:
-        try:
-            cursor = sql.cursor()
-            cursor.execute(query)
-            ans = cursor.fetchall()
-            for i in ans:
-                logging.info(i)
-        finally:
-            cursor.close()
-
-def sql_resource_lookup_key(session, domain, resource_params):
-    """
-    Get the lookup key that identifies the resource from the database.
-
-    Args:
-        session (Session): Open boto3 session.
-        domain (str): VPC such as integration.boss.
-        resource_params (str): Identifies collection, experiment or channel.
-
-    Returns:
-        (str): Lookup key.
-    """
-    collection, experiment, channel = None, None, None
-    resource = resource_params.split("/")
-    
-    if len(resource) == 0:
-        raise Exception("Incorrect number of arguments(Make sure the resource provided has at least a collection to lookup)")
-    else:
-        if len(resource) > 0:
-            collection = resource_params.split("/")[0]
-        if len(resource) > 1:
-            experiment = resource_params.split("/")[1]
-        if len(resource) > 2:
-            channel = resource_params.split("/")[2]
-        elif len(resource) > 3:
-            raise Exception("Only provide /coll/exp/chan")
-
-    coll_query = "SELECT id FROM collection WHERE name = %s"
-    exp_query = "SELECT id FROM experiment WHERE name = %s"
-    chan_query = "SELECT id FROM channel WHERE name = %s"
-
-    with boss_rds.connect_rds(session, domain) as sql:
-        try:
-            cursor = sql.cursor()
-            if collection is not None:
-                cursor.execute(coll_query, (collection,))
-                coll_set = cursor.fetchall()
-                if len(coll_set) != 1:
-                    raise ResourceNotFoundException(
-                        "Can't find collection: {}".format(collection))
-                else:
-                    cuboid_str = "{}&".format(coll_set[0][0])
-                    logging.info("{} collection id: {}".format(collection, coll_set[0][0]))
-            if experiment is not None:
-                cursor.execute(exp_query, (experiment,))
-                exp_set = cursor.fetchall()
-                if len(exp_set) != 1:
-                    raise ResourceNotFoundException(
-                        "Can't find experiment: {}".format(experiment))
-                else:
-                    cuboid_str = cuboid_str + "{}&".format(exp_set[0][0])
-                    logging.info("{} experiment id: {}".format(experiment, exp_set[0][0]))
-            if channel is not None:
-                cursor.execute(chan_query, (channel,))
-                chan_set = cursor.fetchall()
-                if len(chan_set) != 1:
-                    raise ResourceNotFoundException(
-                        "Can't find channel: {}".format(experiment))
-                else:
-                    cuboid_str = cuboid_str + "{}&".format(chan_set[0][0])
-                    logging.info("{} channel id: {}".format(channel, chan_set[0][0]))
-        finally:
-            cursor.close()
-    
-    logging.info("Cuboid key: {} \n".format(cuboid_str))
-    return cuboid_str
-
-def sql_coordinate_frame_lookup_key(session, domain, coordinate_frame):
-    """
-    Get the lookup key that identifies the coordinate fram specified.
-
-    Args:
-        session (Session): Open boto3 session.
-        domain (str): VPC such as integration.boss.
-        coordinate_frame: Identifies coordinate frame.
-
-    Returns:
-        (str): Lookup key.
-    """
-
-    query = "SELECT id FROM coordinate_frame WHERE name = %s"
-
-    logging.info('Tunneling to DB (slow) . . .')
-    with boss_rds.connect_rds(session, domain) as sql:
-        try:
-            cursor = sql.cursor()
-            cursor.execute(query, (coordinate_frame,))
-            coordinate_set = cursor.fetchall()
-            if len(coordinate_set) != 1:
-                raise ResourceNotFoundException(
-                    "Can't find coordinate frame: {}".format(coordinate_frame))
-            else:
-                logging.info("{} coordinate frame id: {}".format(coordinate_frame, coordinate_set[0][0]))
-
-        finally:
-            cursor.close()
-    
-    return coordinate_set[0][0]
-
 COMMANDS = {
-    "sql-list": sql_list,
-    "sql-resource-lookup": sql_resource_lookup_key,
-    "sql-coord-frame-lookup": sql_coordinate_frame_lookup_key,
+    "sql-list": boss_rds.sql_list,
+    "sql-resource-lookup": boss_rds.sql_resource_lookup_key,
+    "sql-coord-frame-lookup": boss_rds.sql_coordinate_frame_lookup_key,
 }
 
 if __name__ == '__main__':
--- lib/boss_rds.py
+++ lib/boss_rds.py
@@ -39,17 +39,6 @@ Fields:
 """
 DbParams = namedtuple('DbParams', ['host', 'port', 'db', 'user', 'password'])
 
-"""
-Container that identifies Boss channel.
-
-Fields:
-    collection (str): Collection name.
-    experiment (str): Experiment name.
-    channel (str): Channel name.
-"""
-ChannelParams = namedtuple(
-    'ChannelParams', ['collection', 'experiment', 'channel'])
-
 def get_mysql_params(session, domain):
     """
     Get MySQL connection info from Vault.
@@ -75,6 +64,16 @@ def get_mysql_params(session, domain):
 
 @contextmanager
 def connect_rds(session, domain):
+    """
+    Context manager with established connection to rds
+
+    Args:
+        session (Session): Open boto3 session.
+        domain (str): VPC such as integration.boss.
+
+    Returns:
+        sql: connection context
+    """
     mysql_params = get_mysql_params(session, domain)
 
     keypair = aws.keypair_lookup(session)
@@ -87,8 +86,131 @@ def connect_rds(session, domain):
                 user=mysql_params.user, password=mysql_params.password, 
                 port=local_port, database=mysql_params.db
             )
-
             yield sql
         finally:
             sql.close()
+            
+def sql_list(session, domain, db_table):
+    """
+    List all the available members of a given sql table.
+
+    Args:
+        session (Session): Open boto3 session.
+        domain (str): VPC such as integration.boss.
+        db_table: Identifies which table members to list.
+
+    Returns:
+        (str): Lookup key.
+    """
+    query = "SELECT * FROM {}".format(db_table)
+
+    logging.info('Tunneling to DB (slow) . . .')
+    with connect_rds(session, domain) as sql:
+        try:
+            cursor = sql.cursor()
+            cursor.execute(query)
+            ans = cursor.fetchall()
+            for i in ans:
+                logging.info(i)
+        finally:
+            cursor.close()
+
+def sql_resource_lookup_key(session, domain, resource_params):
+    """
+    Get the lookup key that identifies the resource from the database.
+
+    Args:
+        session (Session): Open boto3 session.
+        domain (str): VPC such as integration.boss.
+        resource_params (str): Identifies collection, experiment or channel.
+
+    Returns:
+        (str): Lookup key.
+    """
+    collection, experiment, channel = None, None, None
+    resource = resource_params.split("/")
+    
+    if len(resource) == 0:
+        raise Exception("Incorrect number of arguments(Make sure the resource provided has at least a collection to lookup)")
+    else:
+        if len(resource) > 0:
+            collection = resource_params.split("/")[0]
+        if len(resource) > 1:
+            experiment = resource_params.split("/")[1]
+        if len(resource) > 2:
+            channel = resource_params.split("/")[2]
+        elif len(resource) > 3:
+            raise Exception("Only provide /coll/exp/chan")
+
+    coll_query = "SELECT id FROM collection WHERE name = %s"
+    exp_query = "SELECT id FROM experiment WHERE name = %s"
+    chan_query = "SELECT id FROM channel WHERE name = %s"
+
+    with connect_rds(session, domain) as sql:
+        try:
+            cursor = sql.cursor()
+            if collection is not None:
+                cursor.execute(coll_query, (collection,))
+                coll_set = cursor.fetchall()
+                if len(coll_set) != 1:
+                    raise ResourceNotFoundException(
+                        "Can't find collection: {}".format(collection))
+                else:
+                    cuboid_str = "{}&".format(coll_set[0][0])
+                    logging.info("{} collection id: {}".format(collection, coll_set[0][0]))
+            if experiment is not None:
+                cursor.execute(exp_query, (experiment,))
+                exp_set = cursor.fetchall()
+                if len(exp_set) != 1:
+                    raise ResourceNotFoundException(
+                        "Can't find experiment: {}".format(experiment))
+                else:
+                    cuboid_str = cuboid_str + "{}&".format(exp_set[0][0])
+                    logging.info("{} experiment id: {}".format(experiment, exp_set[0][0]))
+            if channel is not None:
+                cursor.execute(chan_query, (channel,))
+                chan_set = cursor.fetchall()
+                if len(chan_set) != 1:
+                    raise ResourceNotFoundException(
+                        "Can't find channel: {}".format(experiment))
+                else:
+                    cuboid_str = cuboid_str + "{}&".format(chan_set[0][0])
+                    logging.info("{} channel id: {}".format(channel, chan_set[0][0]))
+        finally:
+            cursor.close()
+    
+    logging.info("Cuboid key: {} \n".format(cuboid_str))
+    return cuboid_str
+
+def sql_coordinate_frame_lookup_key(session, domain, coordinate_frame):
+    """
+    Get the lookup key that identifies the coordinate fram specified.
+
+    Args:
+        session (Session): Open boto3 session.
+        domain (str): VPC such as integration.boss.
+        coordinate_frame: Identifies coordinate frame.
+
+    Returns:
+        (str): Lookup key.
+    """
+
+    query = "SELECT id FROM coordinate_frame WHERE name = %s"
+
+    logging.info('Tunneling to DB (slow) . . .')
+    with connect_rds(session, domain) as sql:
+        try:
+            cursor = sql.cursor()
+            cursor.execute(query, (coordinate_frame,))
+            coordinate_set = cursor.fetchall()
+            if len(coordinate_set) != 1:
+                raise ResourceNotFoundException(
+                    "Can't find coordinate frame: {}".format(coordinate_frame))
+            else:
+                logging.info("{} coordinate frame id: {}".format(coordinate_frame, coordinate_set[0][0]))
+
+        finally:
+            cursor.close()
+    
+    return coordinate_set[0][0]
     
