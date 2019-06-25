101027f4d30e8ccdb9b95322e3b86e8500b76a5f
Sandy Hider <sandy.hider@jhuapl.edu>
2019-06-25 17:13:39 -0400

    removed second call to generate_lambda_key

--- cloud_formation/configs/dynamolambda.py
+++ cloud_formation/configs/dynamolambda.py
@@ -77,7 +77,7 @@ def create_config(bosslet_config):
     config.add_arg(Arg.String("LambdaCacheExecutionRole", role,
                               "IAM role for " + names.multi_lambda.lambda_))
 
-    lambda_key = generate_lambda_key(bosslet_config)
+    lambda_key = bosslet_config.names.dynamodb_autoscale.zip
     config.add_lambda(DYNAMO_LAMBDA_KEY,
                       names.dynamo_lambda.lambda_,
                       Ref("LambdaCacheExecutionRole"),
