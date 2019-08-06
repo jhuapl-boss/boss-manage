a092cccd1e428bbf6c90ddbd2d53fc4912ef573d
Derek Pryor <Derek.Pryor@jhuapl.edu>
2019-08-06 15:03:37 -0700

    Updated auth security group to explicitly allow internal resources to access it, in case the HTTPS_SUBNET doesn't explicitly allow it

--- cloud_formation/configs/core.py
+++ cloud_formation/configs/core.py
@@ -221,7 +221,9 @@ def create_config(bosslet_config):
     config.add_security_group("AuthSecurityGroup",
                               #names.https.sg, DP XXX: hack until we can get production updated correctly
                               names.auth.sg,
-                              [("tcp", "443", "443", incoming_subnet)])
+                              [("tcp", "443", "443", incoming_subnet), # Allow external requests
+                               ("tcp", "443", "443", Ref("NAT")])      # Allow requests from the endpoint and other
+                                                                       # services contained within the VPC
 
     # Create the internal route table to route traffic to the NAT Bastion
     all_internal_subnets = internal_subnets.copy()
