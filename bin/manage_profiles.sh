9f880e0c9e243a0ea4e1b7068c1542728589eeb3
Derek Pryor <Derek.Pryor@jhuapl.edu>
2019-11-21 14:16:18 -0800

    Added shell script for managing AWS profiles file

---
+++ bin/manage_profiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PROFILE_FILE=${PROFILE_FILE:-"${HOME}/.aws/credentials"}
+
+if [ $# -lt 1 ] ; then
+    echo "Usage: $0 <action>"
+    exit 1
+fi
+
+ACTION=$1
+
+if [ "$ACTION" == "list" ] ; then
+    echo "Current profiles:"
+    grep -E "\[.+\]" $PROFILE_FILE | tr -d "[]" | sed -e 's/_inactive/ (inactive)/'
+    exit 0
+fi
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $0 <activate | deactivate> <profile | all>"
+    exit 1
+fi
+
+PROFILE=$2
+
+case "${ACTION}-${PROFILE}" in
+    activate-all) sed -i -e "s/\[\(.*\)_inactive\]/\[\1\]/" $PROFILE_FILE ;;
+    activate-*) sed -i -e "s/\[${PROFILE}_inactive\]/\[${PROFILE}\]/" $PROFILE_FILE ;;
+    deactivate-all) sed -i -e "s/\[\(.*\)\]/\[\1_inactive\]/" $PROFILE_FILE ;;
+    deactivate-*) sed -i -e "s/\[${PROFILE}\]/\[${PROFILE}_inactive\]/" $PROFILE_FILE ;;
+    *) echo "foobar" ;;
+esac
