d79317301e7d13a7f2381d14beb11044799f4d7a
Derek Pryor <Derek.Pryor@jhuapl.edu>
2019-11-20 23:48:00 -0800

    Updated CF configs to use new lambda build process

--- cloud_formation/configs/dynamolambda.py
+++ cloud_formation/configs/dynamolambda.py
@@ -1,5 +1,3 @@
-# Copyright 2016 The Johns Hopkins University Applied Physics Laboratory
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -28,6 +26,7 @@ import io
 from lib.cloudformation import CloudFormationConfiguration, Arg, Arn, Ref
 from lib import aws
 from lib import constants as const
+from lib import lambdas
 import os
 import json
 import shutil
@@ -35,9 +34,6 @@ import subprocess
 
 # Location of repo with the lambda autoscaler.
 LAMBDA_ROOT_FOLDER = const.repo_path('cloud_formation', 'lambda', 'dynamodb-lambda-autoscale')
-
-# Zip file created by `npm run build`
-LAMBDA_ZIP_FILE = os.path.join(LAMBDA_ROOT_FOLDER, 'dist.zip')
 
 CONFIG_OUTPUT_PATH = os.path.join(LAMBDA_ROOT_FOLDER, 'config.env.production')
 
@@ -58,13 +54,10 @@ def create_config(bosslet_config):
     config.add_arg(Arg.String("LambdaCacheExecutionRole", role,
                               "IAM role for " + names.multi_lambda.lambda_))
 
-    lambda_key = bosslet_config.names.dynamodb_autoscale.zip
     config.add_lambda(DYNAMO_LAMBDA_KEY,
                       names.dynamo_lambda.lambda_,
                       Ref("LambdaCacheExecutionRole"),
-                      s3=(bosslet_config.LAMBDA_BUCKET,
-                          lambda_key,
-                          "index.handler"),
+                      handler="index.handler",
                       timeout=120,
                       memory=128,
                       runtime="nodejs8.10",
@@ -112,11 +105,14 @@ def pre_init(bosslet_config):
     """
     write_config_file(bosslet_config)
     copy_provisioners(bosslet_config)
-    
-    build_lambda(bosslet_config)
 
-    zip_file = os.path.join(LAMBDA_ROOT_FOLDER, LAMBDA_ZIP_FILE)
-    zips_s3_key = upload_to_s3(bosslet_config, zip_file)
+    # Inject the current region into the lambda code
+    region_json = const.repo_path('cloud_formation', 'lambda', 'dynamodb-lambda-autoscale', 'src', 'configuration', 'Region.json')
+    with open(region_json, 'w') as fh:
+        json.dump({'Region': bosslet_config.REGION}, fh)
+    
+    lambdas.load_lambdas_on_s3(bosslet_config,
+                               bosslet_config.names.dynamo_lambda.lambda_,)
 
 
 def write_config_file(bosslet_config):
@@ -164,68 +160,3 @@ def copy_provisioners(bosslet_config):
     src = os.path.join(CONFIG_INPUT_DIR, filename)
     dst = os.path.join(CONFIG_OUTPUT_DIR, PROVISIONER_FILENAME)
     shutil.copy(src, dst)
-
-
-def build_lambda(bosslet_config):
-    """Package lambda in preparation to upload to S3."""
-    install_node_deps()
-    build_node(bosslet_config)
-
-
-def install_node_deps():
-    """npm install NodeJS dependencies."""
-    print('Installing NodeJS dependencies.')
-    args = ('npm', 'install')
-    popen = subprocess.Popen(args, cwd=LAMBDA_ROOT_FOLDER, stdout=subprocess.PIPE)
-    exit_code = popen.wait()
-    output = popen.stdout.read()
-    if not exit_code == 0:
-        print(str(output))
-        raise RuntimeError('Failed to install dependencies.')
-
-
-def build_node(bosslet_config):
-    """Build and package in dist.zip."""
-    print('Packaging NodeJS app.')
-
-    # Inject the current region into the lambda code
-    region_json = const.repo_path('cloud_formation', 'lambda', 'dynamodb-lambda-autoscale', 'src', 'configuration', 'Region.json')
-    with open(region_json, 'w') as fh:
-        json.dump({'Region': bosslet_config.REGION}, fh)
-
-    args = ('npm', 'run', 'build')
-    # Use env vars to pass credentials / region instead of as arguments to build call
-    env = os.environ.copy()
-    env['AWS_REGION'] = bosslet_config.REGION
-    if bosslet_config.PROFILE is not None:
-        env['AWS_PROFILE'] = bosslet_config.PROFILE
-    popen = subprocess.Popen(args, cwd=LAMBDA_ROOT_FOLDER, env=env, stdout=subprocess.PIPE)
-    exit_code = popen.wait()
-    output = popen.stdout.read()
-    if not exit_code == 0:
-        print(str(output))
-        raise RuntimeError('Failed to build Node application.')
-
-
-def upload_to_s3(bosslet_config, zip_file):
-    """Upload the zip file to the correct S3 bucket.
-
-    Args:
-        bosslet_config (BossConfiguration): Bosslet configuration class which contains everything specific to a boss:
-                                            session, bucket, names
-        zip_file (str): Name of zip file.
-
-    """
-    print('Uploading to S3.')
-    bucket = bosslet_config.LAMBDA_BUCKET
-    session = bosslet_config.session
-    key = bosslet_config.names.dynamodb_autoscale.zip
-    s3 = session.client('s3')
-
-    try:
-        s3.create_bucket(Bucket=bucket)
-    except s3.exceptions.BucketAlreadyOwnedByYou:
-        pass # Only us-east-1 will not throw an exception if the bucket already exists
-
-    s3.put_object(Bucket=bucket, Key=key, Body=open(zip_file, 'rb'))
-
