5b37accfffddcfce7f0000f44b451db99c4a16f8
Sandy Hider <sandy.hider@jhuapl.edu>
2019-12-05 11:12:16 -0500

    changes to role so you can see when an update is actually making changes.

--- bin/iam_utils.py
+++ bin/iam_utils.py
@@ -400,7 +400,8 @@ class IamUtils(object):
                         console.error("Problem creating {}: {}".format(resource_type, resource[key]))
                         console.error("\tDetails: {}".format(str(ex)))
             else: # Currently exists, compare and update
-                console.info("Updating {} {}".format(key[:-4], resource[key]))
+                updating = False;
+                console.info("Comparing {} {}".format(key[:-4], resource[key]))
 
                 if resource['Path'] != resource_['Path']:
                     console.warning("Paths differ for {} {}: '{}' != '{}'".format(key,
@@ -413,7 +414,9 @@ class IamUtils(object):
                 if resource_type == 'groups':
                     self.group_update(resource, resource_)
                 elif resource_type == 'roles':
-                    self.role_update(resource, resource_)
+                    updating = updating or self.role_update(resource, resource_)
+                    if updating:
+                        console.info("Updating {} {}".format(key[:-4], resource[key]))
                 elif resource_type == 'policies':
                     self.policy_update(resource, resource_)
 
@@ -552,11 +555,16 @@ class IamUtils(object):
         Args:
             resource (object): Desired IAM Role definition
             resource_ (object): Current IAM Role definition
+
+        Returns:
+            (bool) role was updated.
         """
+        updating = False
         policy = json_dumps(resource['AssumeRolePolicyDocument'])
         policy_ = json_dumps(resource_['AssumeRolePolicyDocument'])
         if policy != policy_:
             console.warning('Role policy document differs')
+            updating = True
             self.iw.update_assume_role_policy(resource['RoleName'],
                                               resource['AssumeRolePolicyDocument'])
 
@@ -565,6 +573,7 @@ class IamUtils(object):
         for policy in resource['RolePolicyList']:
             policy_ = lookup.get(policy['PolicyName'])
             if policy_ is None:
+                updating = True
                 self.iw.put_role_policy(resource['RoleName'],
                                         policy['PolicyName'],
                                         policy['PolicyDocument'])
@@ -572,21 +581,25 @@ class IamUtils(object):
                 document = json_dumps(policy['PolicyDocument'])
                 document_ = json_dumps(policy_)
                 if document != document_:
+                    updating = True
                     self.iw.put_role_policy(resource['RoleName'],
                                             policy['PolicyName'],
                                             policy['PolicyDocument'])
 
         for policy in lookup.keys():
             # AWS has a policy that is not in the desired version, it should be deleted
+            updating = True
             self.iw.delete_role_policy(resource['RoleName'], policy)
 
         for arn in resource['AttachedManagedPolicies']:
             if arn not in resource_['AttachedManagedPolicies']:
+                updating = True
                 self.iw.attach_role_policy(resource["RoleName"], arn)
 
         for arn in resource_['AttachedManagedPolicies']:
             if arn not in resource['AttachedManagedPolicies']:
                 # AWS has a managed policy that is not in the desired version, it should be deleted.
+                updating = True
                 self.iw.detach_role_policy(resource["RoleName"], arn)
 
         lookup = { profile['InstanceProfileName']: profile
@@ -594,6 +607,7 @@ class IamUtils(object):
         for profile in resource['InstanceProfileList']:
             profile_ = lookup.get(profile['InstanceProfileName'])
             if profile_ is None:
+                updating = True
                 self.iw.create_instance_profile(profile['InstanceProfileName'],
                                                 profile['Path'])
                 self.iw.add_role_to_instance_profile(resource['RoleName'],
@@ -608,8 +622,11 @@ class IamUtils(object):
 
         for profile in lookup.keys():
             # AWS has an instance profile that is not in the desired version, it should be deleted
+            updating = True
             self.iw.remove_role_from_instance_profile(resource['RoleName'], profile)
             self.iw.delete_instance_profile(profile)
+
+        return updating
 
     def role_remove(self, resource):
         """Remove the referenced IAM Role
