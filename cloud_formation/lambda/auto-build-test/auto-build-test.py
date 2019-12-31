c1d150bca08e317b6cf8fc63d37af5cf8a767124
Derek Pryor <Derek.Pryor@jhuapl.edu>
2019-12-30 23:29:09 -0800

    Updated auto build test to install spdb test dependencies before running django tests (which run spdb tests)

--- cloud_formation/lambda/auto-build-test/auto-build-test.py
+++ cloud_formation/lambda/auto-build-test/auto-build-test.py
@@ -185,6 +185,7 @@ set +e
 
 #Endpoint tests:
 echo 'Performing tests...'
+$PYTHON ./bastion.py endpoint.auto-build-test.boss ssh-cmd "sudo python3 -m pip install -r /srv/salt/spdb/files/spdb.git/requirements-test.txt"
 $PYTHON ./bastion.py endpoint.auto-build-test.boss ssh-cmd "cd /srv/www/django && python3 manage.py test" # python3 manage.py test -- -c inttest.cfg
 
 #ndingest library
