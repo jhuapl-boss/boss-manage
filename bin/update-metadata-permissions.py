fab4e08d2fc89a1f6bd2bdb3e15798c4c28db5d1
Daniel Xenes <daniel.xenes@jhuapl.edu>
2023-02-15 16:52:13 -0500

    addresses primary groups

--- bin/update-metadata-permissions.py
+++ bin/update-metadata-permissions.py
@@ -3,8 +3,10 @@
 from intern.remote.boss import BossRemote
 from intern.resource.boss.resource import *
 from tqdm.auto import tqdm
+import time
 
 rmt = BossRemote()
+PRIMARY_USER_GROUPS = [x for x in rmt.list_groups() if '-primary' in x]
 
 def confirm_prompt(question: str) -> bool:
     reply = None
@@ -12,29 +14,46 @@ def confirm_prompt(question: str) -> bool:
         reply = input(f"{question} (y/n): ").casefold()
     return (reply == "y")
 
-def update_permissions(resource):
+def update_group_permissions(resource):
     existing_perms = rmt.list_permissions(resource=resource)
     for perm_set in existing_perms:
         new_perms = []
-        if 'read' in perm_set['permissions']:
+        if "read" in perm_set['permissions']:
             new_perms.append('read_metadata')
         if "add" in perm_set['permissions']: 
             # This means that this is a write or admin user for this resource
             new_perms.extend(['add_metadata', 'update_metadata', 'delete_metadata'])
         if new_perms:
-            print(f"Adding {new_perms} for {perm_set['group']} to {resource}")
+            print(f"Adding {new_perms} for {perm_set['group']} to {resource.name}")
             rmt.add_permissions(perm_set['group'], resource=resource, permissions=new_perms)
+
+def update_primary_permissions(resource):
+    for group in PRIMARY_USER_GROUPS:
+        new_perms = []
+        existing_perms = rmt.get_permissions(group, resource)
+
+        if "read" in existing_perms:
+            new_perms.append('read_metadata')
+        if "add" in existing_perms: 
+            new_perms.extend(['add_metadata', 'update_metadata', 'delete_metadata'])
+        if new_perms:
+            print(f"Adding {new_perms} for {group} to {resource.name}")
+            rmt.add_permissions(group, resource=resource, permissions=new_perms)
+
 
 def main():
     for coll_name in tqdm(rmt.list_collections()):
         coll = CollectionResource(coll_name)
-        update_permissions(coll)
+        update_group_permissions(coll)
+        update_primary_permissions(coll)
         for exp_name in rmt.list_experiments(coll_name):
             exp = ExperimentResource(exp_name, coll_name)
-            update_permissions(exp)
+            update_group_permissions(exp)
+            update_primary_permissions(exp)
             for chan_name in rmt.list_channels(coll_name, exp_name):
                 chan = ChannelResource(chan_name, coll_name, exp_name)
-                update_permissions(chan)
+                update_group_permissions(chan)
+                update_primary_permissions(chan)
 
 if __name__ == '__main__':
     if confirm_prompt("""
@@ -42,4 +61,6 @@ Make sure to check your intern.cfg file!
 Are you sure you want to update ALL permissions?
     """
     ):
+        start = time.time()
         main()
+        print(f"Total time: {round(time.time() - start, 2)}")
